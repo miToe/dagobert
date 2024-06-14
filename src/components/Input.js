87a8fe47d308ed00724f54ebad061a4314d8f79d
Nicholas <ntiri97@gmail.com>
2024-06-14 12:34:59 +0200

    inputFild fix

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,5 @@
 import { useState } from "react";
-import {
-  InputContainer,
-  Label,
-  Textarea,
-} from "/src/components/styles/InputStyle.js";
+import { Label, Textarea } from "/src/components/styles/InputField.js";
 
 export default function Input({ label, name, placeholder }) {
   const [isFilled, setIsFilled] = useState(false);
@@ -13,15 +9,18 @@ export default function Input({ label, name, placeholder }) {
   }
 
   return (
-    <InputContainer>
+    <>
       <Label htmlFor={name}>{label}</Label>
+
       <Textarea
+        rows="5"
+        cols="50"
         id={name}
         name={name}
         placeholder={placeholder}
         onBlur={handleBlur}
         className={isFilled ? "filled" : ""} //className is used to apply different styles when the textarea is filled
       />
-    </InputContainer>
+    </>
   );
 }

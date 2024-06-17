231f1efd56987452eb35bc940c610d3cb3761144
Michael Töpfer <micha.toepfer@googlemail.com>
2024-06-17 11:18:58 +0200

    add ohno

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,12 @@ import {
 } from "@/src/components/styles/List";
 import { StyledListItem } from "@/src/components/StyledListItem";
 import SearchBar from "@/src/components/SearchBar";
-
+import { OhNoContainer } from "@/src/components/styles/OhNo";
+import SVGIcon from "../components/SVGIcon";
 
 export default function TransactionList({ transactions, onCurrencySymbol }) {
-  const [filteredTransactions, setFilteredTransactions] = useState(transactions);
+  const [filteredTransactions, setFilteredTransactions] =
+    useState(transactions);
 
   const handleSearchResults = (results) => {
     setFilteredTransactions(results);
@@ -25,13 +27,32 @@ export default function TransactionList({ transactions, onCurrencySymbol }) {
     <ListWrapper>
       <StyledTitle>Transactions</StyledTitle>
       <SearchBar data={transactions} onSearchResults={handleSearchResults} />
-      <StyledList>
-        {sortedTransactions.map((transaction) => (
-          <li key={transaction.id}>
-            <StyledListItem transaction={transaction} onCurrencySymbol={onCurrencySymbol} />
-          </li>
-        ))}
-      </StyledList>
+      {sortedTransactions.length > 0 ? (
+        <StyledList>
+          {sortedTransactions.map((transaction) => (
+            <li key={transaction.id}>
+              <StyledListItem
+                transaction={transaction}
+                onCurrencySymbol={onCurrencySymbol}
+              />
+            </li>
+          ))}
+        </StyledList>
+      ) : (
+        <OhNoContainer>
+          <SVGIcon
+            iconName="ohNo"
+            color="var(--neutrals-dark-gray)"
+            size="115"
+          />
+          <br />
+          <span>
+            <b>No matching results found.</b>
+            <br />
+            Please try again.
+          </span>
+        </OhNoContainer>
+      )}
       <Link href="/transactions/create">Add</Link>
     </ListWrapper>
   );

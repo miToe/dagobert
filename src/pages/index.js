8a5b645688502f20e5b570f164121e62135faa76
Michael Töpfer <micha.toepfer@googlemail.com>
2024-06-18 11:51:48 +0200

    searchbar on fesh main (#68)
    * searchbar on fesh main
    
    * add ohno
    
    * resolve conflict in SVGIcon
    
    * upscale the searchbar
    
    * codestyle

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,57 @@
+import { useState, useCallback } from "react";
 import Link from "next/link";
-import { ListWrapper, StyledList, StyledTitle } from "@/src/components/styles/List";
+import {
+  ListWrapper,
+  StyledList,
+  StyledTitle,
+} from "@/src/components/styles/List";
 import { StyledListItem } from "@/src/components/StyledListItem";
+import SearchBar from "@/src/components/SearchBar";
+import { OhNoContainer } from "@/src/components/styles/OhNo";
+import SVGIcon from "../components/SVGIcon";
 
 export default function TransactionList({ transactions, onCurrencySymbol }) {
-  // Sort transactions by date in descending order
-  const sortedTransactions = transactions.slice().sort(
-    (a, b) => new Date(b.date) - new Date(a.date),
-  );
+  const [filteredTransactions, setFilteredTransactions] =
+    useState(transactions);
 
+  const handleSearchResults = useCallback((results) => {
+    setFilteredTransactions(results);
+  }, []);
+
+  const sortedTransactions = filteredTransactions
+    .slice()
+    .sort((a, b) => new Date(b.date) - new Date(a.date));
 
   return (
     <ListWrapper>
       <StyledTitle>Transactions</StyledTitle>
-      <StyledList>
-        {sortedTransactions.map((transaction) => (
-          <li key={transaction.id}>
-            <StyledListItem transaction={transaction} onCurrencySymbol={onCurrencySymbol} />
-          </li>
-        ))}
-      </StyledList>
+      <SearchBar data={transactions} onSearchResults={handleSearchResults} />
+      {filteredTransactions.length > 0 ? (
+        <StyledList>
+          {sortedTransactions.map((transaction) => (
+            <li key={transaction.id}>
+              <StyledListItem
+                transaction={transaction}
+                onCurrencySymbol={onCurrencySymbol}
+              />
+            </li>
+          ))}
+        </StyledList>
+      ) : (
+        <OhNoContainer>
+          <SVGIcon
+            iconName="ohNo"
+            color="var(--neutrals-dark-gray)"
+            size="115"
+          />
+          <br />
+          <span>
+            <b>No matching results found.</b>
+            <br />
+            Please try again.
+          </span>
+        </OhNoContainer>
+      )}
       <Link href="/transactions/create">Add</Link>
     </ListWrapper>
   );
